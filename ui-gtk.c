029e664379f8073b0317059832f59f8fd2516e7f
Øyvind Kolås <pippin@gimp.org>
2013-10-27 00:51:02 -0400

    docs: name snap gap method; and reformulate its description.
    also reformat README to not exceed 80columns.

--- ui-gtk.c
+++ ui-gtk.c
@@ -873,7 +873,7 @@ g_signal_connect (G_OBJECT (window), "key_press_event", G_CALLBACK (kernagic_key
     gtk_combo_box_text_insert_text (GTK_COMBO_BOX_TEXT (spin_method),
                                     2, "Bearing table (F3)");
     gtk_combo_box_text_insert_text (GTK_COMBO_BOX_TEXT (spin_method),
-                                    3, "gap snap (F4)");
+                                    3, "Snap gap (F4)");
     gtk_widget_set_tooltip_text (spin_method, "F1, F2, F3…");
     gtk_box_pack_start (GTK_BOX (vbox1), spin_method, FALSE, FALSE, 2);
   }
